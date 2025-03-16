920b147ec7177b93709db142c2383b0817a67770
Purbanka <purbankaghosh1708@gmail.com>
2025-03-16 22:09:43 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import {clerkMiddleware} from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
@@ -9,17 +12,26 @@ import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
-import { use } from "react";
-
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials: true
+}));
 
+app.use(express.json());
 app.use(clerkMiddleware());
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname,"tmp"),
+    createParentPath: true,
+    limits: {fileSize: 10 * 1024 * 1024},
+}));
 
 app.use("/api/users",userRoutes);
 app.use("/api/admin",adminRoutes);
@@ -27,6 +39,10 @@ app.use("/api/auth",authRoutes);
 app.use("/api/songs",songRoutes);
 app.use("/api/albums",albumRoutes);
 app.use("/api/stats",statRoutes);
+
+app.use((err,req,res,next)=>{
+    res.status(500).json({message:process.env.NODE_ENV === "production" ? "Internal Server Error" : err.message});
+});
 
 
 app.listen(PORT,()=>{console.log("server is running on port"+PORT);
